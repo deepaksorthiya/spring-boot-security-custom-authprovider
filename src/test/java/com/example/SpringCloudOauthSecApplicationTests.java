86ccb68d3db79ac9f5266bd24e950d52f1821281
Deepak Katariya <deepak.sorthiya@gmail.com>
2021-06-09 09:16:40 +0530

    Update SpringCloudOauthSecApplicationTests.java
    testcases update

--- src/test/java/com/example/SpringCloudOauthSecApplicationTests.java
+++ src/test/java/com/example/SpringCloudOauthSecApplicationTests.java
@@ -1,16 +1,24 @@
 package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringCloudOauthSecApplicationTests {
 
+	@Autowired
+	ApplicationContext applicationContext;
+
 	@Test
 	public void contextLoads() {
+		assertNotNull(applicationContext, "application context should not be null");
 	}
 
 }
